c8311217fd05de1025d40b788fe6508b1d4028de
Venz01 <venzjethric.lapana123@gmail.com>
2026-04-11 22:47:25 +0800

    user management fix

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -412,12 +412,10 @@
     <!-- Hidden Forms -->
     <form id="approveForm" method="POST" style="display: none;">
         @csrf
-        @method('PATCH')
     </form>
 
     <form id="rejectForm" method="POST" style="display: none;">
         @csrf
-        @method('PATCH')
     </form>
 
     <form id="statusForm" method="POST" style="display: none;">
